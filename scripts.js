6b04c3f91f66997cb912e94aac29cde898ebbfd4
Weronika Musial <jozefinamusial@gmail.com>
2017-03-13 22:08:58 +0100

    poprawione

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var animal = 'Zielone słonie';
-animal = animal.toUpperCase();
-parrots = text.replace('Papugi', animal);
+var animal = 'Zielone słonie'.toUpperCase();
+var parrots = text.replace('Papugi', animal);
 
-halfSentence = parrots.slice(0, 45);
+halfSentence = parrots.substr(0, parrots.length /2);
 console.log(halfSentence);
-
-halfSentence2 = parrots.substr(0, parrots.length/2);
-console.log(halfSentence2);
