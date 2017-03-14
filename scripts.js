db333fee2213c49ae5e02a70e34140a0fa7833f6
Weronika Musial <jozefinamusial@gmail.com>
2017-03-14 12:55:02 +0100

    poprawione2

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na 
 var animal = 'Zielone słonie'.toUpperCase();
 var parrots = text.replace('Papugi', animal);
 
-halfSentence = parrots.substr(0, parrots.length /2);
+var halfSentence = parrots.substr(0, parrots.length /2);
 console.log(halfSentence);
